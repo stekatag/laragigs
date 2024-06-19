cf07aeee56ba18909c73c65684c58c9240819265
Stefan Gogov <stefangogov@proton.me>
2024-06-19 14:17:03 +0300

    chore: Add edit and delete routes and methods for listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -48,4 +48,39 @@ class ListingController extends Controller {
 
         return redirect('/')->with('message', 'Listing created successfully!');
     }
+
+    // Show edit form
+    public function edit(Listing $listing) {
+        return view('listings.edit', [
+            'listing' => $listing,
+        ]);
+    }
+
+    // Store listing data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => ['required', 'url'],
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated successfully!');
+    }
+
+    // Delete listing
+    public function destroy(Listing $listing) {
+        $listing->delete();
+
+        return redirect('/')->with('message', 'Listing deleted successfully!');
+    }
 }
